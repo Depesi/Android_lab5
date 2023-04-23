a07268d24dfc3fcce3049b95a3b70284c358caee
John Doe <johndoe@example.com>
2023-04-23 12:25:41 +0300

    addBindingExpressionToTheScoreAndTheWordCouns

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -68,13 +68,6 @@ class GameFragment : Fragment() {
         // Setup a click listener for the Submit and Skip buttons.
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
-
-        // Observe the scrambledCharArray LiveData, passing in the LifecycleOwner and the observer.
-        viewModel.score.observe(viewLifecycleOwner,
-            { newScore -> binding.score.text = getString(R.string.score, newScore) })
-        viewModel.currentWordCount.observe(viewLifecycleOwner,
-            { newWordCount -> binding.wordCount.text =
-                    getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS) })
     }
 
     /*
